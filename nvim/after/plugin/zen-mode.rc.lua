f2b32ed56543b73c24df81cdda45a7d7a6f311c8
Brenden Callahan <brendenkcallahan@gmail.com>
2023-01-22 18:58:07 -0800

    .

--- nvim/after/plugin/zen-mode.rc.lua
+++ nvim/after/plugin/zen-mode.rc.lua
@@ -2,6 +2,7 @@ local status, zenMode = pcall(require, "zen-mode")
 if (not status) then return end
 
 zenMode.setup {
+  width = 1
 }
 
 vim.keymap.set('n', '<leader>zm', '<cmd>ZenMode<cr>', { silent = true })
