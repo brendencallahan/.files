121a31d85d9089e0a844d19117879a4e02fe8143
Brenden Callahan <brendenkcallahan@gmail.com>
2023-01-23 16:17:39 -0800

    Add Lualine rounded corners

--- nvim/after/plugin/lualine.rc.lua
+++ nvim/after/plugin/lualine.rc.lua
@@ -5,8 +5,8 @@ lualine.setup {
   options = {
     icons_enabled = true,
     theme = 'onedark', --onedark
-    section_separators = { left = '', right = '' },
-    component_separators = { left = '', right = '' },
+    section_separators = { left = '', right = '' },
+    component_separators = { left = '', right = '' },
     disabled_filetypes = {}
   },
   sections = {
