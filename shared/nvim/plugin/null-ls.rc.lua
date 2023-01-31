9e3966bafe14a38097536aaa0e1a3173095952e9
Brenden Callahan <brendenkcallahan@gmail.com>
2023-01-31 15:40:47 -0800

    Changed from eslint_d to vscode-eslint-server

--- shared/nvim/plugin/null-ls.rc.lua
+++ shared/nvim/plugin/null-ls.rc.lua
@@ -5,9 +5,9 @@ null_ls.setup {
   sources = {
     null_ls.builtins.completion.luasnip,
     null_ls.builtins.formatting.prettierd,
-    null_ls.builtins.code_actions.eslint_d,
+    null_ls.builtins.code_actions.eslint,
     null_ls.builtins.code_actions.gitsigns,
-    null_ls.builtins.diagnostics.eslint_d.with({
+    null_ls.builtins.diagnostics.eslint.with({
       diagnostics_format = '[eslint] #{m}\n(#{c})'
     }),
     null_ls.builtins.diagnostics.zsh
