1e5ab5e6c23da84cba97bb8dd3fcdaffdf18dc3b
Brenden Callahan <brendenkcallahan@gmail.com>
2023-02-11 15:17:31 -0800

    Added formatting for python via black

--- shared/nvim/plugin/null-ls.rc.lua
+++ shared/nvim/plugin/null-ls.rc.lua
@@ -5,6 +5,7 @@ null_ls.setup {
   sources = {
     null_ls.builtins.completion.luasnip,
     null_ls.builtins.formatting.prettierd,
+    null_ls.builtins.formatting.black,
     null_ls.builtins.code_actions.eslint_d,
     null_ls.builtins.code_actions.gitsigns,
     null_ls.builtins.diagnostics.eslint_d.with({
