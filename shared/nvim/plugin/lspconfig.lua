44758a3dc69948aa89bdce06c29c91997d928c7f
Brenden Callahan <brendenkcallahan@gmail.com>
2023-02-12 16:31:48 -0800

    Better javascript support

--- shared/nvim/plugin/lspconfig.lua
+++ shared/nvim/plugin/lspconfig.lua
@@ -1,5 +1,4 @@
---vim.lsp.set_log_level("debug")
-
+-- vim.lsp.set_log_level("debug")
 local status, nvim_lsp = pcall(require, "lspconfig")
 if (not status) then return end
 
@@ -30,35 +29,34 @@ protocol.CompletionItemKind = {
   '', -- Struct
   '', -- Event
   'ﬦ', -- Operator
-  '', -- TypeParameter
+  '' -- TypeParameter
 }
 
 -- Set up completion using nvim_cmp with LSP source
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-nvim_lsp.tailwindcss.setup {
-  capabilities = capabilities
-}
+nvim_lsp.tailwindcss.setup {capabilities = capabilities}
 
-nvim_lsp.cssls.setup {
-  capabilities = capabilities
-}
+nvim_lsp.cssls.setup {capabilities = capabilities}
 
 nvim_lsp.pylsp.setup {
-  filetypes = { "python" },
-  cmd = { "pyright-langserver", "--stdio"},
+  filetypes = {"python"},
+  cmd = {"pyright-langserver", "--stdio"},
   capabilities = capabilities
 }
 
 nvim_lsp.rust_analyzer.setup {
-  filetypes = { "rust" },
-  cmd = { "rustup", "run", "stable", "rust-analyzer" },
+  filetypes = {"rust"},
+  cmd = {"rustup", "run", "stable", "rust-analyzer"},
   capabilities = capabilities
 }
 
 nvim_lsp.tsserver.setup {
-  filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
-  cmd = { "typescript-language-server", "--stdio" },
+  filetypes = {
+    "javascript", "javascriptreact", "javascript.jsx", "typescript",
+    "typescriptreact", "typescript.tsx"
+  },
+  cmd = {"typescript-language-server", "--stdio"},
   capabilities = capabilities
 }
 
@@ -66,63 +64,54 @@ nvim_lsp.lua_ls.setup {
   capabilities = capabilities,
   settings = {
     Lua = {
-      format = {
-        enable = false
-      },
+      format = {enable = false},
       diagnostics = {
         -- Get the language server to recognize the `vim` global
-        globals = { 'vim' },
+        globals = {'vim'}
       },
 
       workspace = {
         -- Make the server aware of Neovim runtime files
         library = vim.api.nvim_get_runtime_file("", true),
         checkThirdParty = false
-      },
-    },
-  },
+      }
+    }
+  }
 }
 
 -- Make it look pretty :)
-vim.lsp.handlers['textdocument/hover'] = vim.lsp.with(
-  vim.lsp.handlers.hover,
-  {
-    border = 'rounded',
-    virtual_text = { spacing = 2, prefix = "●" },
-  }
-)
+vim.lsp.handlers['textdocument/hover'] =
+    vim.lsp.with(vim.lsp.handlers.hover, {
+      border = 'rounded',
+      virtual_text = {spacing = 2, prefix = "●"}
+    })
 
-vim.lsp.handlers['textdocument/signaturehelp'] = vim.lsp.with(
-  vim.lsp.handlers.signature_help,
-  {
-    border = 'rounded',
-    virtual_text = { spacing = 2, prefix = "●" },
-  }
-)
+vim.lsp.handlers['textdocument/signaturehelp'] =
+    vim.lsp.with(vim.lsp.handlers.signature_help, {
+      border = 'rounded',
+      virtual_text = {spacing = 2, prefix = "●"}
+    })
 
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-  vim.lsp.diagnostic.on_publish_diagnostics, {
-    border = 'rounded',
-    underline = true,
-    update_in_insert = false,
-    virtual_text = { spacing = 2, prefix = "●" },
-    severity_sort = true,
-  }
-)
+vim.lsp.handlers["textDocument/publishDiagnostics"] =
+    vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+      border = 'rounded',
+      underline = true,
+      update_in_insert = false,
+      virtual_text = {spacing = 2, prefix = "●"},
+      severity_sort = true
+    })
 
 -- Diagnostic symbols in the sign column (gutter)
-local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+local signs = {Error = " ", Warn = " ", Hint = " ", Info = " "}
 for type, icon in pairs(signs) do
   local hl = "DiagnosticSign" .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+  vim.fn.sign_define(hl, {text = icon, texthl = hl, numhl = ""})
 end
 
 vim.diagnostic.config({
-  virtual_text = {
-    prefix = '●'
-  },
+  virtual_text = {prefix = '●'},
   update_in_insert = true,
   float = {
-    source = "always", -- Or "if_many"
-  },
+    source = "always" -- Or "if_many"
+  }
 })
--- shared/nvim/plugin/null-ls.rc.lua
+++ shared/nvim/plugin/null-ls.rc.lua
@@ -7,7 +7,6 @@ null_ls.setup {
     null_ls.builtins.formatting.prettierd,
     null_ls.builtins.formatting.lua_format,
     null_ls.builtins.formatting.black,
-    -- null_ls.builtins.formatting.lua_format,
     null_ls.builtins.code_actions.gitsigns,
     null_ls.builtins.code_actions.eslint_d,
     null_ls.builtins.diagnostics.eslint_d.with({
