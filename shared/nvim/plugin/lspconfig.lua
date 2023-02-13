1c609eb93315d443647dd898713cad9d7877f490
Brenden Callahan <brendenkcallahan@gmail.com>
2023-02-12 19:09:54 -0800

    Fixed lua formatting'

--- shared/nvim/plugin/lspconfig.lua
+++ shared/nvim/plugin/lspconfig.lua
@@ -35,19 +35,19 @@ protocol.CompletionItemKind = {
 -- Set up completion using nvim_cmp with LSP source
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-nvim_lsp.tailwindcss.setup {capabilities = capabilities}
+nvim_lsp.tailwindcss.setup { capabilities = capabilities }
 
-nvim_lsp.cssls.setup {capabilities = capabilities}
+nvim_lsp.cssls.setup { capabilities = capabilities }
 
 nvim_lsp.pylsp.setup {
-  filetypes = {"python"},
-  cmd = {"pyright-langserver", "--stdio"},
+  filetypes = { "python" },
+  cmd = { "pyright-langserver", "--stdio" },
   capabilities = capabilities
 }
 
 nvim_lsp.rust_analyzer.setup {
-  filetypes = {"rust"},
-  cmd = {"rustup", "run", "stable", "rust-analyzer"},
+  filetypes = { "rust" },
+  cmd = { "rustup", "run", "stable", "rust-analyzer" },
   capabilities = capabilities
 }
 
@@ -56,7 +56,7 @@ nvim_lsp.tsserver.setup {
     "javascript", "javascriptreact", "javascript.jsx", "typescript",
     "typescriptreact", "typescript.tsx"
   },
-  cmd = {"typescript-language-server", "--stdio"},
+  cmd = { "typescript-language-server", "--stdio" },
   capabilities = capabilities
 }
 
@@ -64,10 +64,18 @@ nvim_lsp.lua_ls.setup {
   capabilities = capabilities,
   settings = {
     Lua = {
-      format = {enable = false},
+      format = {
+        enable = true,
+        defaultConfig = {
+          indent_style = "space",
+          indent_size = "2",
+          tab_width = "2",
+          continuation_indent = "2",
+        }
+      },
       diagnostics = {
         -- Get the language server to recognize the `vim` global
-        globals = {'vim'}
+        globals = { 'vim' }
       },
 
       workspace = {
@@ -81,35 +89,35 @@ nvim_lsp.lua_ls.setup {
 
 -- Make it look pretty :)
 vim.lsp.handlers['textdocument/hover'] =
-    vim.lsp.with(vim.lsp.handlers.hover, {
-      border = 'rounded',
-      virtual_text = {spacing = 2, prefix = "●"}
-    })
+  vim.lsp.with(vim.lsp.handlers.hover, {
+    border = 'rounded',
+    virtual_text = { spacing = 2, prefix = "●" }
+  })
 
 vim.lsp.handlers['textdocument/signaturehelp'] =
-    vim.lsp.with(vim.lsp.handlers.signature_help, {
-      border = 'rounded',
-      virtual_text = {spacing = 2, prefix = "●"}
-    })
+  vim.lsp.with(vim.lsp.handlers.signature_help, {
+    border = 'rounded',
+    virtual_text = { spacing = 2, prefix = "●" }
+  })
 
 vim.lsp.handlers["textDocument/publishDiagnostics"] =
-    vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-      border = 'rounded',
-      underline = true,
-      update_in_insert = false,
-      virtual_text = {spacing = 2, prefix = "●"},
-      severity_sort = true
-    })
+  vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+    border = 'rounded',
+    underline = true,
+    update_in_insert = false,
+    virtual_text = { spacing = 2, prefix = "●" },
+    severity_sort = true
+  })
 
 -- Diagnostic symbols in the sign column (gutter)
-local signs = {Error = " ", Warn = " ", Hint = " ", Info = " "}
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
 for type, icon in pairs(signs) do
   local hl = "DiagnosticSign" .. type
-  vim.fn.sign_define(hl, {text = icon, texthl = hl, numhl = ""})
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 end
 
 vim.diagnostic.config({
-  virtual_text = {prefix = '●'},
+  virtual_text = { prefix = '●' },
   update_in_insert = true,
   float = {
     source = "always" -- Or "if_many"
--- shared/nvim/plugin/null-ls.rc.lua
+++ shared/nvim/plugin/null-ls.rc.lua
@@ -5,7 +5,6 @@ null_ls.setup {
   sources = {
     null_ls.builtins.completion.luasnip,
     null_ls.builtins.formatting.prettierd,
-    null_ls.builtins.formatting.lua_format,
     null_ls.builtins.formatting.black,
     null_ls.builtins.code_actions.gitsigns,
     null_ls.builtins.code_actions.eslint_d,
