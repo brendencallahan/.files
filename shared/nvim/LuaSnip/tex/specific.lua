20c5a7fe4100a1e5fa0e8d3a631effc906aef4e1
Brenden Callahan <brendenkcallahan@gmail.com>
2023-02-19 09:11:23 -0800

    Seperate tex snips into diff files'

---
+++ shared/nvim/LuaSnip/tex/specific.lua
@@ -0,0 +1,21 @@
+local ls = require 'luasnip'
+local s = ls.snippet
+-- local sn = ls.snippet_node
+local t = ls.text_node
+local i = ls.insert_node
+-- local f = ls.function_node
+-- local c = ls.choice_node
+-- local d = ls.dynamic_node
+-- local r = ls.restore_node
+local fmt = require("luasnip.extras.fmt").fmt
+local fmta = require("luasnip.extras.fmt").fmta
+-- local rep = require("luasnip.extras").rep
+-- local types = require 'luasnip.util.types'
+
+ls.add_snippets("tex", {
+  s({ trig = "specific"},
+    {
+      t("This is a specific file"),
+    }
+  ),
+})
