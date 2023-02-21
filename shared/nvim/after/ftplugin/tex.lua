60b0828a0c1d53c9bbcede8668f0305e543d6fb4
Brenden Callahan <brendenkcallahan@gmail.com>
2023-02-21 13:44:56 -0800

    Support for latex inkscape figures

--- shared/nvim/after/ftplugin/tex.lua
+++ shared/nvim/after/ftplugin/tex.lua
@@ -1,6 +1,9 @@
 vim.wo.foldexpr = 'vimtex#fold#level(v:lnum)'
 vim.wo.foldtext = 'vimtex#fold#text()'
 
+vim.cmd[[inoremap <C-f> <Esc>: silent exec '.!inkscape-figures create "'.getline('.').'" "'.b:vimtex.root.'/figures/"'<CR><CR>:w<CR>]]
+vim.cmd[[nnoremap <C-f> : silent exec '!inkscape-figures edit "'.b:vimtex.root.'/figures/" > /dev/null 2>&1 &'<CR><CR>:redraw!<CR>]]
+
 -- vim.cmd[[
 -- nmap <leader>ll :w<cr> :!pdflatex %:r.tex && bibtex %:r.aux && pdflatex %:r.tex && pdflatex %:r.tex && rm %:r.aux %:r.log %:r.blg %:r.bbl<cr>
 -- ]]
