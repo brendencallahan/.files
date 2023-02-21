29368f9c53a1c2a2ffc37fbe8ae7efed62ec61ba
Brenden Callahan <brendenkcallahan@gmail.com>
2023-02-21 13:07:18 -0800

    Add manual latex compile keybinds

--- shared/nvim/after/ftplugin/tex.lua
+++ shared/nvim/after/ftplugin/tex.lua
@@ -1,2 +1,10 @@
 vim.wo.foldexpr = 'vimtex#fold#level(v:lnum)'
 vim.wo.foldtext = 'vimtex#fold#text()'
+
+-- vim.cmd[[
+-- nmap <leader>ll :w<cr> :!pdflatex %:r.tex && bibtex %:r.aux && pdflatex %:r.tex && pdflatex %:r.tex && rm %:r.aux %:r.log %:r.blg %:r.bbl<cr>
+-- ]]
+--
+-- vim.cmd[[
+-- nmap <leader>lv :!zathura %:r.pdf > /dev/null 2>&1 &<cr><cr>
+-- ]]
