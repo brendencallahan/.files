7766bfa1592ec4a87f9b6b1f28fd87042de8f5ce
Brenden Callahan <brendenkcallahan@gmail.com>
2023-03-07 06:27:59 -0800

    Lualine update

--- shared/nvim/after/plugin/gitsigns.lua
+++ shared/nvim/after/plugin/gitsigns.lua
@@ -10,6 +10,13 @@ gitsigns.setup {
     row = 0,
     col = 1
   },
+  current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+  current_line_blame_opts = {
+    virt_text = true,
+    virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+    delay = 100,
+    ignore_whitespace = false,
+  },
   on_attach = function(bufnr)
     local gs = package.loaded.gitsigns
 
--- shared/nvim/after/plugin/lualine.lua
+++ shared/nvim/after/plugin/lualine.lua
@@ -14,20 +14,34 @@ lualine.setup {
     theme = custom_theme, --onedark
     section_separators = { left = '', right = '' },
     component_separators = { left = '', right = '' },
-    disabled_filetypes = {}
+    disabled_filetypes = {},
+    -- globalstatus = true,
   },
   sections = {
     lualine_a = { 'mode' },
-    lualine_b = { 'branch' },
-    lualine_c = { {
-      'filename',
-      file_status = true, -- displays file status (readonly status, modified status)
-      path = 0 -- 0 = just filename, 1 = relative path, 2 = absolute path
-    } },
+    lualine_b = {
+      'branch',
+      'diff'
+    },
+    lualine_c = {
+      {
+        'filename',
+        file_status = true, -- displays file status (readonly status, modified status)
+        path = 0            -- 0 = just filename, 1 = relative path, 2 = absolute path
+      },
+    },
     lualine_x = {
-      { 'diagnostics', sources = { "nvim_diagnostic" }, symbols = { error = ' ', warn = ' ', info = ' ',
-        hint = ' ' } },
-      'encoding',
+      {
+        'diagnostics',
+        sources = { "nvim_diagnostic" },
+        symbols = {
+          error = ' ',
+          warn = ' ',
+          info = ' ',
+          hint = ' '
+        }
+      },
+      -- 'encoding',
       'filetype'
     },
     lualine_y = { 'progress' },
@@ -39,12 +53,16 @@ lualine.setup {
     lualine_c = { {
       'filename',
       file_status = true, -- displays file status (readonly status, modified status)
-      path = 1 -- 0 = just filename, 1 = relative path, 2 = absolute path
+      path = 1            -- 0 = just filename, 1 = relative path, 2 = absolute path
     } },
     lualine_x = { 'location' },
     lualine_y = {},
     lualine_z = {}
   },
   tabline = {},
-  extensions = { 'nvim-tree' }
+  extensions = {
+    'nvim-tree',
+    'nvim-dap-ui',
+    'toggleterm'
+  }
 }
